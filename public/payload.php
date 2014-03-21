7ba4fbcfd33c83e546fe4f84b1de2dc27d5780c4
Michael Norris <github@michaelnorris.co.uk>
2014-03-21 22:28:11 +0000

    edit payload to show output

--- public/payload.php
+++ public/payload.php
@@ -1,7 +1,8 @@
 <?php
 
 if ($_POST['payload']) {
-    shell_exec('cd ~/public_html/domains/moodeebee/dev/ && git reset --hard HEAD && git pull');
+    $output = shell_exec('cd ~/public_html/domains/moodeebee/dev/ && git reset --hard HEAD && git pull');
+    echo $output;
 }
 
 ?>
